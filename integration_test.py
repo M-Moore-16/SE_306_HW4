3e98ecbadb9896456fe3ca45c5137e02b7746501
Mason <m.moore.16000@gmail.com>
2026-04-28 18:16:37 -0500

    added more integration tests

--- integration_test.py
+++ integration_test.py
@@ -2,7 +2,7 @@ import pytest
 import calculator
 
 # test multiplication and addition integration        
-class TestTogether:
+class TestMultiAndAdd:
     
     @pytest.mark.parametrize("a,b,expected", [
         (5, 5, 30),  # Positive numbers
@@ -10,5 +10,17 @@ class TestTogether:
         (0, 5, 5),  # Zero case
         (7, -2, -16),  # Mixed case
     ])
-    def test_together(self, a, b, expected):
+    def test_multi_and_add(self, a, b, expected):
         assert calculator.Add(calculator.Multi(a,b),b) == expected
+        
+# test multiplication and addition integration        
+class TestDivAndSub:
+    
+    @pytest.mark.parametrize("a,b,expected", [
+        (20, 5, -1),  # Positive numbers
+        (-8, 2, -6),  # Negative numbers
+        (0, 5, -5),  # Zero case
+        (12, -2, -4),  # Mixed case
+    ])
+    def test_div_and_sub(self, a, b, expected):
+        assert calculator.Sub(calculator.Div(a,b),b) == expected
