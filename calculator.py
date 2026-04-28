01fa4a0a488ca14142ee15ef118a39a0769c4066
Mason <m.moore.16000@gmail.com>
2026-04-28 13:35:06 -0500

    Initial Commit - Made a basic calculator to test.

---
+++ calculator.py
@@ -0,0 +1,30 @@
+"""
+A simple calculator
+made for SE_306 Homework 4
+to test GitHub actions
+"""
+
+# Addition A + B
+def Add(A,B):
+    C = A + B
+    return C
+
+# Subtraction A - B
+def Sub(A,B):
+    C = A - B
+    return C
+
+# Multiplication A * B
+def Multi(A,B):
+    C = A * B
+    return C
+
+# Division A / B
+def Div(A,B):
+    C = A / B
+    return C
+
+# Modulus
+def Mod(A,B):
+    C = A % B
+    return C
---
+++ main.py
@@ -0,0 +1,8 @@
+import calculator
+
+
+print(calculator.Add(1,1))
+print(calculator.Sub(1,1))
+print(calculator.Multi(1,1))
+print(calculator.Div(1,1))
+print(calculator.Mod(1,1))
